3ca32316000b3c3ced2f951af8edfcbb70bff860
duc thanh <ducthanhpg@gmail.com>
2024-03-21 15:24:04 +0700

    init base

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,49 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Checkbox,
+  CheckboxGroup,
+  Image,
+} from "@nextui-org/react";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="mt-8 flex flex-col gap-4">
+        <Card className="py-4">
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <p className="text-tiny uppercase font-bold">Daily Mix</p>
+            <small className="text-default-500">12 Tracks</small>
+            <h4 className="font-bold text-large">Frontend Radio</h4>
+          </CardHeader>
+          <CardBody className="overflow-visible py-2">
+            <Image
+              alt="Card background"
+              className="object-cover rounded-xl"
+              src="https://picsum.photos/id/237/200/300"
+              width={270}
+            />
+          </CardBody>
+        </Card>
+        <CheckboxGroup
+          className="mt-5"
+          orientation="horizontal"
+          color="secondary"
+          defaultValue={["buenos-aires", "san-francisco"]}
+        >
+          <Checkbox value="buenos-aires">Buenos Aires</Checkbox>
+          <Checkbox value="sydney">Sydney</Checkbox>
+          <Checkbox value="san-francisco">San Francisco</Checkbox>
+          <Checkbox value="london">London</Checkbox>
+          <Checkbox value="tokyo">Tokyo</Checkbox>
+        </CheckboxGroup>
 
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+        <Button color="secondary" size="lg">
+          Press me
+        </Button>
+      </div>
+    </div>
+  );
 }
